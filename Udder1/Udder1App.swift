26c6c15b3b42856d8fa21111cafdbcfba26b8f5d
idofof <idofwww@gmail.com>
2023-01-07 12:56:54 +0800

    Initial Commit

---
+++ Udder1/Udder1App.swift
@@ -0,0 +1,17 @@
+//
+//  Udder1App.swift
+//  Udder1
+//
+//  Created by id on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Udder1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
